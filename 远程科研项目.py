22265c4a5a3a1814824a6905ade403562906c0e0
Arthur-Liu98 <75744236+Arthur-Liu98@users.noreply.github.com>
2020-12-10 00:06:17 +0800

    Add files via upload

---
+++ 远程科研项目.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+print(tf.__version__)
+
+
+# In[3]:
+
+
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+
+# In[4]:
+
+
+print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
+
+
+# In[5]:
+
+
+print(train_data[0])
+
+
+# In[6]:
+
+
+len(train_data[0]), len(train_data[1])
+
+
+# In[7]:
+
+
+# 一个映射单词到整数索引的词典
+word_index = imdb.get_word_index()
+
+# 保留第一个索引
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+# In[8]:
+
+
+decode_review(train_data[0])
+
+
+# In[9]:
+
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=word_index["<PAD>"],
+                                                       padding='post',
+                                                       maxlen=256)
+
+
+# In[10]:
+
+
+len(train_data[0]), len(train_data[1])
+
+
+# In[11]:
+
+
+print(train_data[0])
+
+
+# In[12]:
+
+
+# 输入形状是用于电影评论的词汇数目（10,000 词）
+vocab_size = 10000
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation='relu'))
+model.add(keras.layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+
+# In[13]:
+
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+# In[14]:
+
+
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+
+# In[15]:
+
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+
+# In[16]:
+
+
+results = model.evaluate(test_data,  test_labels, verbose=2)
+
+print(results)
+
+
+# In[17]:
+
+
+history_dict = history.history
+history_dict.keys()
+dict_keys(['loss', 'accuracy', 'val_loss', 'val_accuracy'])
+
+
+# In[18]:
+
+
+import matplotlib.pyplot as plt
+
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# “bo”代表 "蓝点"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b代表“蓝色实线”
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+
+# In[19]:
+
+
+plt.clf()   # 清除数字
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
